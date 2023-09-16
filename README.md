cf3d74a1891da274f2a0fe8d53c497c65f42edc9
Elijah Soladoye <shodown96@gmail.com>
2023-09-16 22:44:06 +0100

    fix

---
+++ README.md
@@ -0,0 +1,9 @@
+# GraphBlog
+### [Live Site](https://grpahblog.vercel.app/)
+
+## Introduction
+This is a code repository for the corresponding video tutorial. 
+
+With featured and recent posts, categories. full markdown articles, author information, comments, and much more, this fully responsive CMS Blog App is the best Blog Application that you can currently find on YouTube. And what's best of all is that you and your clients can manage the blog from a dedicated Content Management System.
+
+You'll also learn how to work with GraphCMS. GraphCMS is a headless content management system based on GraphQL technology enabling seamless integration with any application.
--- components/Author.jsx
+++ components/Author.jsx
@@ -4,19 +4,19 @@ import Image from 'next/image';
 import { grpahCMSImageLoader } from '../util';
 
 const Author = ({ author }) => (
-  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
-    <div className="absolute left-0 right-0 -top-14">
+  <div className="text-center mt-32 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+    <div className="absolute left-0 right-0 -top-20 flex justify-center">
       <Image
         unoptimized
         loader={grpahCMSImageLoader}
         alt={author.name}
-        height="100px"
-        width="100px"
+        height={150}
+        width={150}
         className="align-middle rounded-full"
         src={author.photo.url}
       />
     </div>
-    <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+    <h3 className="text-white mt-8 mb-4 text-xl font-bold">{author.name}</h3>
     <p className="text-white text-ls">{author.bio}</p>
   </div>
 );
