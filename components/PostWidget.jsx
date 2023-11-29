5db86f8926f3d20dbaba940fe90552a96d2338e1
Elijah Soladoye <shodown96@gmail.com>
2023-11-30 00:11:31 +0100

    env file

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -25,7 +25,7 @@ const PostWidget = ({ categories, slug }) => {
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
       {relatedPosts.map((post, index) => (
-        <div key={index} className="flex items-center w-full mb-4">
+        <div key={index} className="flex items-center w-full mb-4 font-bold">
           <div className="w-16 flex-none">
             <img src={post.featuredImage.url}
               className="h-14 w-14 object-cover rounded-full"
--- next.config.js
+++ next.config.js
@@ -1,12 +1,32 @@
-module.exports = {
+// module.exports = {
+//   reactStrictMode: true,
+//   eslint: {
+//     // Warning: This allows production builds to successfully complete even if
+//     // your project has ESLint errors.
+//     ignoreDuringBuilds: true,
+//   },
+
+//   images: {
+//     domains: ['media.graphassets.com']
+//   }
+// };
+
+// Base plugin config needed by Next.js
+const { pluginoptions } = require('@mightymeld/runtime');
+
+const nextConfig = {
+  experimental: {
+    swcPlugins: [['@mightymeld/runtime/swc-plugin-mightymeld', pluginoptions()]]
+  },
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-
   images: {
     domains: ['media.graphassets.com']
   }
 };
+
+module.exports = process.env.MIGHTYMELD ? nextConfig : {};
